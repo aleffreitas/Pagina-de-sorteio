9cc9a03e7df66d9d83ba6495c0da3175690e83a9
Alef Freitas <alef.freitas07@gmail.com>
2021-10-29 12:44:21 -0300

    Adicionado funcções JS e iniciado estilização CSS.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let pessoas = ["Carlos","Maria","José","Carla","Antônio","Joana","Pamela"];
+let pessoas = [];
+let premios = [];
+
+
+/*let quantidadePessoas = Number(prompt("Informe a quantidade de pessoas"));
+let quantidadePremios = Number(prompt("Informe a quantidade de prêmios"));
+
+for(let i = 0; i < quantidadePessoas; i++) {
+    pessoas.push(prompt(`Informe o nome da Pessoa ${i+1}`));
+}
+
+for(let i = 0; i < quantidadePremios; i++) {
+    premios.push(prompt(`Informe o nome do Premio ${i+1}`));
+}*/
 
 function sortear(){
     
@@ -6,4 +19,8 @@ function sortear(){
     let numeroSorteado = Math.floor(Math.random() * numeroPessoas);
     document.getElementById("numero").innerHTML = numeroSorteado +1;
     document.getElementById("nome").innerHTML = pessoas[numeroSorteado];
+
+    let numeroPremios = premios.length;
+    let numeroSorteado2 = Math.floor(Math.random() * numeroPremios);
+    document.getElementById("final").innerHTML = [`Parabéns ${pessoas[numeroSorteado]}! Você ganhou: ${premios[numeroSorteado2]}`];
 }
