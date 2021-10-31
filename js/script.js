585efc2476130bd874c1b45ffb5a1aadf5af46fd
Alef Freitas <alef.freitas07@gmail.com>
2021-10-30 21:02:20 -0300

    Finalizado App de sorteio :tada:

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 let pessoas = [];
 let premios = [];
 
-
-/*let quantidadePessoas = Number(prompt("Informe a quantidade de pessoas"));
-let quantidadePremios = Number(prompt("Informe a quantidade de prêmios"));
-
-for(let i = 0; i < quantidadePessoas; i++) {
-    pessoas.push(prompt(`Informe o nome da Pessoa ${i+1}`));
+function remover(){
+    document.getElementById("alerta").innerHTML = "";
 }
-
-for(let i = 0; i < quantidadePremios; i++) {
-    premios.push(prompt(`Informe o nome do Premio ${i+1}`));
-}*/
 
 function adicionarPessoa(){
     let pessoa = document.getElementById("nomePessoa");
     pessoas.push(pessoa.value);
+    document.getElementById("alerta").innerHTML = `${pessoa.value} Adicionado`;    
+    pessoa.value = "";    
+
+    setTimeout(remover,1500);
 }
 
 function adicionarPremio(){
     let premio = document.getElementById("nomePremio");
     premios.push(premio.value);
+    document.getElementById("alerta").innerHTML = `${premio.value} Adicionado`; 
+    premio.value = "";
+
+    setTimeout(remover,1500);
 }
 
-function sortear(){
+function sortear(){  
+    
+    if(pessoas.length == 0 || premios.length == 0){
+        alert("Insira os Nomes e os Prêmios antes de clicar em Sortear!");
+    }else{
     
     let numeroPessoas = pessoas.length;
     let numeroSorteado = Math.floor(Math.random() * numeroPessoas);
-    document.getElementById("numero").innerHTML = numeroSorteado +1;
-    document.getElementById("nome").innerHTML = pessoas[numeroSorteado];
+    document.getElementById("numero").innerHTML = `Número sorteado: ${numeroSorteado +1}`;
+    document.getElementById("nome").innerHTML = `Nome: ${pessoas[numeroSorteado]}`;
 
     let numeroPremios = premios.length;
-    let numeroSorteado2 = Math.floor(Math.random() * numeroPremios);
-    document.getElementById("final").innerHTML = [`Parabéns ${pessoas[numeroSorteado]}! Você ganhou: ${premios[numeroSorteado2]}`];
+    let numeroSorteado2 = Math.floor(Math.random() * numeroPremios);   
+    
+    document.getElementById("final").innerHTML = [`Parabéns ${pessoas[numeroSorteado]} ! Prêmio: ${premios[numeroSorteado2]}`];
+    }
 }
